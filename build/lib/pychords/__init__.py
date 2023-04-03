5d389105b9e805e36342d08b14271849dc0b3db7
Ivo Melse <ivo@melse.info>
2023-04-03 12:27:52 +0200

    removed overhead

--- build/lib/pychords/__init__.py
+++
@@ -1,6 +0,0 @@
-from pychords.chords import *
-from pychords.chord_types import *
-from pychords.modes import *
-from pychords.notes import *
-from pychords.scales import *
-from pychords.transpose import *
